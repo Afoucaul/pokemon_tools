42babc9ace7581cce5a442e1d1609dd613368872
armand <armand.foucault@telecom-bretagne.eu>
2018-10-13 23:28:28 +0200

    Allowed cross-origin requests

--- pokemon_tools/__init__.py
+++ pokemon_tools/__init__.py
@@ -1,7 +1,10 @@
 from importlib import import_module
 import flask
+import flask_cors
 
 version = "0.0.0"
 
 app = flask.Flask(__name__)
+flask_cors.CORS(app)
 map_editor = import_module(".map_editor", __name__)
+engine = import_module(".engine", __name__)
