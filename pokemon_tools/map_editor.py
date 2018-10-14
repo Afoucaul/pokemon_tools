699dfc8c3cd90d5d6112291a4ac8dd505befd836
armand <armand.foucault@telecom-bretagne.eu>
2018-10-14 13:25:42 +0200

    Implemented uploaded image reading

--- pokemon_tools/map_editor.py
+++ pokemon_tools/map_editor.py
@@ -1,5 +1,30 @@
+from flask import request
+from PIL import Image
+import io
+from .engine import world
 from . import app
+from . import utils
 
-@app.route("/hi")
+
+@app.route("/map")
 def greet():
     return "hello"
+
+
+@app.route("/map/convert", methods=['POST'])
+def convert():
+     received_world = request.form['map']
+     return b'hello'
+
+
+@app.route("/map/tileset/prepare", methods=['POST'])
+def prepare_tileset():
+    image_source = request.files['image']
+    data = image_source.stream.read()
+
+    image_descriptor = io.BytesIO(data)
+    image = Image.open(image_descriptor)
+    image.show()
+
+    return ""
+
