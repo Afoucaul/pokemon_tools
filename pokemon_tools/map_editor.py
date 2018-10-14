ce79a02b676d2b3fce184ff1dfaa374088405f18
armand <armand.foucault@telecom-bretagne.eu>
2018-10-14 15:59:53 +0200

    Fixed image sending

--- pokemon_tools/map_editor.py
+++ pokemon_tools/map_editor.py
@@ -1,9 +1,9 @@
-from flask import request
+from flask import request, jsonify
 from PIL import Image
 import io
 from .engine import world
 from . import app
-from . import utils
+import base64
 
 
 @app.route("/map")
@@ -20,11 +20,22 @@ def convert():
 @app.route("/map/tileset/prepare", methods=['POST'])
 def prepare_tileset():
     image_source = request.files['image']
+    width = int(request.form['tileWidth'])
+    height = int(request.form['tileHeight'])
+
     data = image_source.stream.read()
-
     image_descriptor = io.BytesIO(data)
-    image = Image.open(image_descriptor)
-    image.show()
+    original = Image.open(image_descriptor)
 
-    return ""
+    tiles = []
+    for i in range(original.width // width):
+        for j in range(original.height // height):
+            rect = ((width)*i, (height)*j, (width)*(i+1) - 1, (height)*(j+1) - 1)
+            tile = original.crop(rect)
 
+            output = io.BytesIO()
+            tile.save(output, format='PNG')
+            png = base64.b64encode(output.getvalue()).decode('ascii')
+            tiles.append(png)
+
+    return jsonify({'tiles': tiles})
